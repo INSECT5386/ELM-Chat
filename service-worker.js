6efbc742a96c6720433ab6a4ecaa105c58d8275f
INSECT5386 <shinnara.yuchan@gmail.com>
2025-08-05 16:37:52 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,13 @@
 // 루트 또는 public/ 아래에 service-worker.js
-const CACHE_NAME = 'covec-cache-v13';
+const CACHE_NAME = 'covec-cache-v14';
 const urlsToCache = [
-  '/CoVec-Web/',
-  '/CoVec-Web/index.html',
-  '/CoVec-Web/css/style.css',
-  '/CoVec-Web/js/script.js',
-  '/CoVec-Web/manifest.json',
-  '/CoVec-Web/img/icon-192.png',
-  '/CoVec-Web/img/icon-512.png',
+  '/ELM-Chat/',
+  '/ELM-Chat/index.html',
+  '/ELM-Chat/css/style.css',
+  '/ELM-Chat/js/script.js',
+  '/ELM-Chat/manifest.json',
+  '/ELM-Chat/img/icon-192.png',
+  '/ELM-Chat/img/icon-512.png',
   'https://cdn.jsdelivr.net/npm/marked/marked.min.js',
   'https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.8.0/build/highlight.min.js',
   'https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.8.0/build/styles/atom-one-dark.min.css'
